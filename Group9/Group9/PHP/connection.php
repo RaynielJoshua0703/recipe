43ed00675933fa0ef070f1c3b60e129b4126b1f3
RaynielJoshua0703 <161727044+RaynielJoshua0703@users.noreply.github.com>
2024-07-08 13:53:29 +0800

    Update connection.php

--- Group9/Group9/PHP/connection.php
+++ Group9/Group9/PHP/connection.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_HOST', 'database-2.cp40g6m2ixdz.ap-southeast-2.rds.amazonaws.com');
+    define('DB_USER', 'admin');
+    define('DB_PASS', 'hatdogka123');
     define('DB_NAME', 'recipe_repository');
 
     $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
